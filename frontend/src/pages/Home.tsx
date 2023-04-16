cbe3787f76b18fd3f73dd992739341e82a04eaae
UserKazun <kazuusjlo8@gmail.com>
2023-04-16 14:10:51 +0900

    add components, page, router

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Card from "components/Card"
+
+const Home = () => {
+    return <LoginPage />
+}
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Card>
+                <p>Card hear</p>
+            </Card>
+        </div>
+    )
+}
+
+export default Home
