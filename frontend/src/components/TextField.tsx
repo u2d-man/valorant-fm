72a93f0a8cd458d713ea875a44ffc85bfddc5d93
UserKazun <kazuusjlo8@gmail.com>
2023-04-23 10:06:14 +0900

    update textfield

--- frontend/src/components/TextField.tsx
+++ frontend/src/components/TextField.tsx
@@ -1,26 +1,37 @@
-import { ChangeEvent } from "react";
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
 interface Props {
     placeholder: string
+    value: string
+    setValue: (newValue: string) => void
     type: string
     label: string
-    onChange?:  (event: ChangeEvent<HTMLInputElement>) => void
+    inputProps?: InputProps
 }
+
+type InputProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>
 
 const TextField = ({
     placeholder,
+    value,
+    setValue,
     type,
     label,
-    onChange = (_) => undefined,
-}: Props) => {
+    inputProps
+}: Props & InputProps) => {
     return (
         <div>
             <label className="block mt-5 mb-2 text-sm font-medium text-gray-900">{ label }</label>
             <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                type={type}
-                placeholder={placeholder}
-                onChange={onChange}
+                type={ type }
+                { ...inputProps }
+                value={ value }
+                placeholder={ placeholder }
+                onChange={ e => setValue(e.target.value) }
             />
         </div>
     )
