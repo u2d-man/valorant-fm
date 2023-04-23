8c50ce9ceeec272381cbb9e63e7d9909a911b5c0
UserKazun <kazuusjlo8@gmail.com>
2023-04-23 10:08:42 +0900

    update postRegister

--- frontend/src/lib/apis.tsx
+++ frontend/src/lib/apis.tsx
@@ -1,11 +1,14 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 class Apis {
-    async postRegister() {
-        await axios.post<void>(
-            '/api/register',
-            {},
-        )
+    async postRegister(req: postRegisterRequest, axiosonfig?: AxiosRequestConfig) {
+        const data = new FormData()
+        data.append('id', req.id)
+        data.append('password', req.password)
+        await axios.post<void>(`/api/register`, data, {
+            headers: { 'content-type': 'multipart/form-data' },
+            ...axiosonfig
+        })
     }
 
     async postAuth() {
@@ -18,3 +21,8 @@ class Apis {
 
 const apis = new Apis()
 export default apis
+
+export interface postRegisterRequest {
+    id: string
+    password: string
+}
