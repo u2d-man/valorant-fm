17c867da0abf1c444c7265cce9bd5611709a4e85
UserKazun <kazuusjlo8@gmail.com>
2023-04-28 20:39:14 +0900

    add comment

--- src/Application/Handlers/RegisterHandler.php
+++ src/Application/Handlers/RegisterHandler.php
@@ -11,6 +11,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Log\LoggerInterface;
 
+// user register
 class RegisterHandler
 {
     public function __construct(
