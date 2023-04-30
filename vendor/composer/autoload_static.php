50c676fbe236265b562e6d87c09f28f02d4877fe
UserKazun <developer.kazu0941@gmail.com>
2023-04-30 20:14:33 +0900

    add session manage

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,8 +8,8 @@ class ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff
 {
     public static $files = array (
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
-        '9b38cf48e83f5d8f60375221cd213eee' => __DIR__ . '/..' . '/phpstan/phpstan/bootstrap.php',
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
+        '9b38cf48e83f5d8f60375221cd213eee' => __DIR__ . '/..' . '/phpstan/phpstan/bootstrap.php',
         'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
@@ -200,6 +200,10 @@ class ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/..' . '/bryanjhv/slim-session/src',
     );
 
     public static $prefixesPsr0 = array (
@@ -921,6 +925,7 @@ class ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit051038d83f4736cc9e3b3467aeb650ff::$classMap;
 
