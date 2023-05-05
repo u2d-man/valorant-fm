99596119b8215740d35b3997357e29128c398c2f
UserKazun <developer.kazu0941@gmail.com>
2023-05-05 20:13:02 +0900

    display get images

--- frontend/src/components/ImageGrid.tsx
+++ frontend/src/components/ImageGrid.tsx
@@ -1,14 +1,20 @@
+import { useEffect, useState } from "react"
 import ImageDisplay from "./ImageDisplay"
+import apis, { GetimageFilesResponse } from "lib/apis"
 
 const ImageGrid = () => {
-    const imageUrl = ['jett.jpg', 'jett.jpg', 'jett.jpg', 'jett.jpg', 'jett.jpg', 'jett.jpg']
-    const itemList = imageUrl.map((item, index) => {
-        return <ImageDisplay imageName={ item } key={ index } />
-    })
+    const [images, setImages] = useState<GetimageFilesResponse>()
+    useEffect(() => {
+        const fetchImages = async () => {
+            setImages(await apis.getImageFiles())
+        }
+        fetchImages()
+    }, [setImages])
 
     return (
         <div className="grid grid-cols-4 gap-4">
-            { itemList }
+            { images?.data.map((image, index) =>
+                <ImageDisplay imageName={ image } key={ index } />) }
         </div>
     )
 }
